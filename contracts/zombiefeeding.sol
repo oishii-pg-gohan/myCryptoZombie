7a1772cbca87a9a0b694d2fc09d8b567b4c2720a
oishi-pg-gohan <oishii.pg.gohan@gmail.com>
2021-08-12 18:34:54 +0900

    first commit CryptoZombieのCourse2-Chapter8まで実施済み

---
+++ contracts/zombiefeeding.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
+    }
+}
