16b162c69f80332e1b9a459ce6296cceb999c65b
oishi-pg-gohan <oishii.pg.gohan@gmail.com>
2021-08-13 00:25:02 +0900

    Course2-Chapter11 clear. 外部コントラクタのインターフェース初期化処理を実装。 外部コントラクトとしてCryptoKittiesのコントラクトアドレスを使用。（CryptoZombieで指定してもらったやつ）

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -20,7 +20,38 @@ contract KittyInterface {
         );
 }
 
+// クリプトキティーズのgetKitty()関数のインターフェースを用意
+// function getKitty(uint256 _id) external view returns (
+//     bool isGestating,
+//     bool isReady,
+//     uint256 cooldownIndex,
+//     uint256 nextActionAt,
+//     uint256 siringWithId,
+//     uint256 birthTime,
+//     uint256 matronId,
+//     uint256 sireId,
+//     uint256 generation,
+//     uint256 genes
+// ) {
+//     Kitty storage kit = kitties[_id];
+
+//     // if this variable is 0 then it's not gestating
+//     isGestating = (kit.siringWithId != 0);
+//     isReady = (kit.cooldownEndBlock <= block.number);
+//     cooldownIndex = uint256(kit.cooldownIndex);
+//     nextActionAt = uint256(kit.cooldownEndBlock);
+//     siringWithId = uint256(kit.siringWithId);
+//     birthTime = uint256(kit.birthTime);
+//     matronId = uint256(kit.matronId);
+//     sireId = uint256(kit.sireId);
+//     generation = uint256(kit.generation);
+//     genes = kit.genes;
+// }
+
 contract ZombieFeeding is ZombieFactory {
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; // 上記クリプトキティーズのコントラクトアドレス
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
     function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
