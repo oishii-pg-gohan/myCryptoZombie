f5abcdd94abd36df28f16fe8f00748b10e703b87
oishi-pg-gohan <oishii.pg.gohan@gmail.com>
2021-08-12 20:06:08 +0900

    solidityのバージョン指定を修正
    VSCodeのプラグイン：solidityだと、最低0.5.16以上でないとコンパイルエラーになる。
    また、引数にもデータ保持を種類を指定する必要がある。

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.5.16;
 
 contract ZombieFactory {
     event NewZombie(uint256 zombieId, string name, uint256 dna);
@@ -17,19 +17,23 @@ contract ZombieFactory {
     mapping(address => uint256) ownerZombieCount;
 
     // 下の関数定義を編集せよ
-    function _createZombie(string _name, uint256 _dna) private {
+    function _createZombie(string memory _name, uint256 _dna) private {
         uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint256) {
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
         uint256 rand = uint256(keccak256(_str));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint256 randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
