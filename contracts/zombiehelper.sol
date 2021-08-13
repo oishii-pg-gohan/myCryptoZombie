c312a79ac6d1c3ba750e0afc00136d9f7d50e2cc
oishi-pg-gohan <oishii.pg.gohan@gmail.com>
2021-08-13 10:57:36 +0900

    Course3-Chapter11 clear. memoryでの配列の作成。storageはガス代がかかるので、出来るだけmemoryで変数、配列を保持するように心掛けること。

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -28,5 +28,8 @@ contract ZombieHelper is ZombieFeeding {
         external
         view
         returns (uint256[] memory)
-    {}
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        return result;
+    }
 }
