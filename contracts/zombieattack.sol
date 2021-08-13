85b79c1d4508ffade27f15ba6a993b045daf7e09
oishi-pg-gohan <oishii.pg.gohan@gmail.com>
2021-08-13 16:51:42 +0900

    Course4-Chapter4 clear. 乱数生成に関して。 完全に安全な方法ではないが、今回のTutorial上では特に被害は出ないため、keccak256を利用して乱数生成を行っている。

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -2,4 +2,13 @@ pragma solidity ^0.5.16;
 
 import "./zombiehelper.sol";
 
-contract ZombieBattle is ZombieHelper {}
+contract ZombieBattle is ZombieHelper {
+    uint256 randNonce = 0;
+
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce++;
+        return
+            uint256(keccak256(abi.encodePacked(now, msg.sender, randNonce))) %
+            _modulus;
+    }
+}
