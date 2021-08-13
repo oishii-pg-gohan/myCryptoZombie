d7a11e85b04d0afa3cfae627518b0e6b54635934
oishi-pg-gohan <oishii.pg.gohan@gmail.com>
2021-08-13 10:14:09 +0900

    Course3-Chapter8 clear. 引数を持つ関数修飾子の実装。

---
+++ contracts/zombiehelper.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.16;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+}
